1536d877596e0a44ee6de51b595c3e068ba83dbb
Simon Maggini <simonmaggini@gmail.com>
2026-01-03 13:23:32 +0100

    faet: continued with resume

--- app/page.tsx
+++ app/page.tsx
@@ -34,32 +34,41 @@ export default function Home() {
         <Row>
           <Column width="half">
             <Box color="yellow" border="left" title="-">
-              <MathBlock>{"\\int f(ax+b)\\,dx = \\frac{1}{a}F(ax+b) + C"}</MathBlock>
+              <MathBlock>
+                {"\\int f(ax+b)\\,dx = \\frac{1}{a}F(ax+b) + C"}
+              </MathBlock>
             </Box>
           </Column>
           <Column width="half">
-            <Box color="green" border="left" title="Integrazione di funzioni razionali">
-              <MathBlock>{"\\int \\frac{f'(x)}{f(x)}\\,dx = \\ln |f(x)| + C"}</MathBlock>
+            <Box
+              color="green"
+              border="left"
+              title="Integrazione di funzioni razionali"
+            >
+              <MathBlock>
+                {"\\int \\frac{f'(x)}{f(x)}\\,dx = \\ln |f(x)| + C"}
+              </MathBlock>
             </Box>
           </Column>
           <Column width="half">
             <Box color="blue" border="left" title="Integrazione per parti">
-              <MathBlock>{"\\int u'(x)v(x)\\,dx = u(x)v(x) - \\int u(x)v'(x)\\,dx"}</MathBlock>
+              <MathBlock>
+                {"\\int u'(x)v(x)\\,dx = u(x)v(x) - \\int u(x)v'(x)\\,dx"}
+              </MathBlock>
             </Box>
           </Column>
           <Column width="half">
             <Box color="red" border="left" title="-">
-              <MathBlock>{"\\int f(g(x))g'(x)\\,dx = \\int f(u)\\,du"}</MathBlock>
+              <MathBlock>
+                {"\\int f(g(x))g'(x)\\,dx = \\int f(u)\\,du"}
+              </MathBlock>
             </Box>
           </Column>
         </Row>
 
-
         <Theorem title="Area racchiusa tra due curve">
           <MathBlock>{"A = \\int_a^b |f(x) - g(x)| dx"}</MathBlock>
         </Theorem>
-
-
 
         <Note>
           Ricorda: la primitiva è definita a meno di una costante{" "}
@@ -69,10 +78,11 @@ export default function Home() {
 
       <Section title="Curve parametriche">
         <Definition term="Curve parametrica">
-          Una curva parametrica è una funzione <Math>{"r(t) = (x(t), y(t))"}</Math> da <Math>{"[a, b]"}</Math> a <Math>{"\\mathbb{R}^2"}</Math>. <br />
+          Una curva parametrica è una funzione{" "}
+          <Math>{"r(t) = (x(t), y(t))"}</Math> da <Math>{"[a, b]"}</Math> a{" "}
+          <Math>{"\\mathbb{R}^2"}</Math>. <br />
           L&apos;area è data da: <Math>{"\\int_a^b g(t) * f'(t) dt"}</Math>
         </Definition>
-
 
         <FormulaTable
           headers={["Funzioni", "Curve parametriche"]}
@@ -86,21 +96,10 @@ export default function Home() {
             },
           ]}
         />
-
       </Section>
 
       {/* SOLIDI DI ROTAZIONE */}
       <Section title="Solidi di rotazione">
-        <Theorem title="Baricentro con asse x">
-          <Row>
-            <Column width="half">
-              <MathBlock>{"x_G = \\frac{1}{A} \\int_a^b x(t) f(x) dx"}</MathBlock>
-            </Column>
-            <Column width="half">
-              <MathBlock>{"y_G = \\frac{1}{2A} \\int_a^b f(x)^2 dx"}</MathBlock>
-            </Column>
-          </Row>
-        </Theorem>
         <FormulaTable
           headers={["Funzioni", "Curve parametriche"]}
           rows={[
@@ -121,6 +120,19 @@ export default function Home() {
           ]}
         />
 
+        <Theorem title="Baricentro con asse x">
+          <Row>
+            <Column width="half">
+              <MathBlock>
+                {"x_G = \\frac{1}{A} \\int_a^b x(t) f(x) dx"}
+              </MathBlock>
+            </Column>
+            <Column width="half">
+              <MathBlock>{"y_G = \\frac{1}{2A} \\int_a^b f(x)^2 dx"}</MathBlock>
+            </Column>
+          </Row>
+        </Theorem>
+
         <Theorem title="Teorema di Pappo">
           <Row>
             <Column width="third">
@@ -129,13 +141,64 @@ export default function Home() {
             </Column>
             <Column width="two-thirds">
               <Definition term="r">
-                Raggio di rotazione puo essere preso sottraendo il baricentro dalla distanza tra il punto di rotazione e l&apos;asse di rotazione.
-                Oppure se non perpendicolare all&apos;asse di rotazione si puo usare: <br />
-                <Math>{"d(P, r) = \\frac{|ax_P + by_P + c|}{\\sqrt{a^2 + b^2}} = \\frac{|y_P - (m * x_P + q)|}{\\sqrt{1 + m^2}}"}</Math>
+                Raggio di rotazione puo essere preso sottraendo il baricentro
+                dalla distanza tra il punto di rotazione e l&apos;asse di
+                rotazione. Oppure se non perpendicolare all&apos;asse di
+                rotazione si puo usare: <br />
+                <Math>
+                  {
+                    "d(P, r) = \\frac{|ax_P + by_P + c|}{\\sqrt{a^2 + b^2}} = \\frac{|y_P - (m * x_P + q)|}{\\sqrt{1 + m^2}}"
+                  }
+                </Math>
               </Definition>
             </Column>
           </Row>
-          
+        </Theorem>
+      </Section>
+
+      <Section title="Equazioni differenziali">
+        <Row>
+          <Column width="half">
+            <Box color="red" border="left" title="EDO I ordine lineare">
+              <Math>{"y' + p(x)y = q(x)"}</Math>
+              <MathBlock>
+                {"y = e^{-\\mu(x)} \\int q(x) e^{\\mu(x)}\\,dx + C"}
+              </MathBlock>
+            </Box>
+          </Column>
+          <Column width="half">
+            <Box color="yellow" border="left" title="Variabili separabili">
+              <Math>{"y' = f(x) \\cdot g(y)"}</Math>
+              <MathBlock>{"\\int \\frac{dy}{g(y)} = \\int f(x)\\,dx"}</MathBlock>
+            </Box>
+          </Column>
+        </Row>
+
+        <Theorem title="Teorema di Cachy">
+          <Row>
+            <Column width="half">
+              <MathBlock>{"\\frac{dy}{dx} = f(x)g(y)"}</MathBlock>
+            </Column>
+            <Column width="half">
+              <MathBlock>{"\\int \\frac{1}{g(y)} dy = \\int f(x)\\,dx"}</MathBlock>
+            </Column>
+          </Row>
+        </Theorem>
+
+        <Theorem title="EDO II ordine a coefficienti costanti">
+          Per <Math>{"y'' + ay' + by = 0"}</Math>, equazione caratteristica{" "}
+          <Math>{"\\lambda^2 + a\\lambda + b = 0"}</Math>:
+          <ul>
+            <li>
+              Radici reali distinte: <Math>{"y = C_1 e^{\\lambda_1 x} + C_2 e^{\\lambda_2 x}"}</Math>
+            </li>
+            <li>
+              Radice doppia: <Math>{"y = (C_1 + C_2 x) e^{\\lambda x}"}</Math>
+            </li>
+            <li>
+              Radici complesse: <Math>{"y = e^{\\alpha x}(C_1 \\cos\\beta x + C_2 \\sin\\beta x)"}</Math>
+            </li>
+          </ul>
         </Theorem>
       </Section>
 
