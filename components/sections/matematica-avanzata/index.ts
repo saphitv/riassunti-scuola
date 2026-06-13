c740b7f0516bd9c7dee2e3348f66fa224b64eec8
Simon Maggini <simonmaggini@gmail.com>
2026-06-13 18:16:03 +0200

    Remove obsolete determinant and rank sections
    - Delete the determinant and rank matrix study notes
    - Remove both sections from the matemática avanzata export hub

--- components/sections/matematica-avanzata/index.ts
+++ components/sections/matematica-avanzata/index.ts
@@ -1,6 +1,4 @@
 export { MatriciVettoriSection } from "./MatriciVettoriSection";
-export { RangoMatriciSection } from "./RangoMatriciSection";
-export { DeterminantiSection } from "./DeterminantiSection";
 export { AutovaloriAutospaziSection } from "./AutovaloriAutospaziSection";
 export { DiagonalizzazioneSection } from "./DiagonalizzazioneSection";
 export { SistemiEquazioniDifferenzialiSection } from "./SistemiEquazioniDifferenzialiSection";
